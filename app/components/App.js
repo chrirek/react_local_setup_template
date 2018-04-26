a0248e3931aaf8e2d9aab4d9eed014ce16ae3fe9
Christian Rekdal <rekdal.christian@gmail.com>
2018-04-26 14:12:53 +0200

    Added a template for react default local development

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.component {
+  render () {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+module.exports = App;
